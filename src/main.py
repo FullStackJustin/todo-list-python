e66b1211b7ef958fa2342f876cb01e43a0c4c202
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-05-10 01:09:11 +0000

    working

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Starships
 #from models import Person
 
 app = Flask(__name__)
@@ -39,7 +39,26 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/characters', methods=['GET'])
+def handle_characters():
+    characters_query = Characters.query.all()
+    all_characters =list(map(lambda character: character.serialize(), characters_query))
+    return jsonify(all_characters), 200
+
+
+@app.route('/characters/<int:characters_id>', methods=['GET'])
+def handle__character(characters_id):
+    user1 = Characters.query.get(characters_id)
+    return jsonify(user1.serialize()), 200
+
+@app.route('/starships', methods=['GET'])
+def handle_starships():
+    starships_query = Starships.query.all()
+    all_starships =list(map(lambda starships: starships.serialize(), starships_query))
+    return jsonify(all_starships), 200
+
+
 # this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    if __name__ == '__main__':
+        PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -14,6 +14,38 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "email": self.email
             # do not serialize the password, its a security breach
+        }
+class Characters(db.Model):
+    __tablename__ = 'Characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    height = db.Column(db.String(120), unique=False, nullable=False)
+    mass = db.Column(db.Integer, unique=False, nullable=False)
+    hair_color = db.Column(db.String(120), unique=False, nullable=False)
+    skin_color = db.Column(db.String(120), unique=False, nullable=False)
+    eye_color = db.Column(db.String(120), unique=False, nullable=False)
+    birth_year = db.Column(db.String(10), unique=False, nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year
+        }
+
+class Starships(db.Model):
+    __tablename__ = 'Starships'
+    id = db.Column(db.Integer, primary_key=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "model": self.model
         }
